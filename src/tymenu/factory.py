b4ade631873b1d2715e4ea62b8d7b7811e199515
Alexander Tygesen <atygesen@outlook.dk>
2022-06-12 12:08:53 +0200

    Log static path

--- src/tymenu/factory.py
+++ src/tymenu/factory.py
@@ -1,10 +1,14 @@
 """The main app constructor"""
 
-from flask import Flask
+from flask import Flask, url_for
+import logging
 
 
 from .config import get_config
 from .resources import init_plugins
+
+
+logger = logging.getLogger(__name__)
 
 
 def create_app(config_name: str) -> Flask:
@@ -25,5 +29,9 @@ def create_app(config_name: str) -> Flask:
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(menu_blueprint)
+
+    # static path
+    static = url_for("static", filename="styles.css")
+    logger.info("Serving from static path: %s", static)
 
     return app
